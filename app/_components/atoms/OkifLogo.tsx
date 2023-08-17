00f8724917fcfd1bcf48b7036ac3be5709b564ed
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-08-18 03:21:18 +0800

    fix : 5 footer semantic typo and image src

--- app/_components/atoms/OkifLogo.tsx
+++ app/_components/atoms/OkifLogo.tsx
@@ -2,13 +2,12 @@ interface OkifLogoProps {
   size: number;
 }
 import Image from "next/image";
-import Logo from "../../../public/icon/Okif.svg";
 
 const OkifLogo = (props: OkifLogoProps) => {
   return (
     <Image
       priority={true}
-      src={Logo}
+      src={"/icon/Okif.svg"}
       alt={`Okif's Logo`}
       width={props.size}
       height={props.size}
--- app/_components/organisms/Footer.tsx
+++ app/_components/organisms/Footer.tsx
@@ -1,46 +1,60 @@
 import Logo from "../molecules/Logo";
-import Mail_outline from "../../../public/icon/mail_outline.svg";
-import Place from "../../../public/icon/place.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-darker flex p-[72px_172px] flex-col w-screen gap-16">
-      <section className="flex justify-between items-start">
-        <div className="flex flex-col gap-8 text-xs">
+    <footer className="bg-primary-darker flex p-[72px_172px] text-white flex-col w-screen gap-16">
+      <main className="flex justify-between items-start">
+        <section className="flex flex-col gap-8 text-xs">
           <Logo variant={"white"} />
-          <div className="flex flex-col gap-4">
-            <div className="gap-[10px] flex items-start">
-              <Image src={Place} alt="" />
+          <main className="flex flex-col gap-4">
+            <section className="gap-[10px] flex items-start">
+              <Image width={18} height={18} src="/icon/place.svg" alt="" />
               <span className="text-text-s">
                 Gedung Student Center Lt. 1 Fakultas Teknik
                 <br /> Universitas Hasanuddin, Gowa 92171
               </span>
-            </div>
-            <div className="gap-[10px] flex items-start">
-              <Image src={Mail_outline} alt="" />
+            </section>
+            <section className="gap-[10px] flex items-start">
+              <Image
+                width={18}
+                height={18}
+                src="/icon/mail_outline.svg"
+                alt=""
+              />
 
               <span className="text-text-s">okifftuh@gmail.com</span>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 items-end">
+            </section>
+          </main>
+        </section>
+        <section className="flex flex-col gap-4 items-end">
           <span className="text-text-l font-bold">
-            Unit Kegiatan Mahasiswa Despanartemen
+            Unit Kegiatan Mahasiswa Departemen
           </span>
-          <span className="text-text-m">Coder Institute</span>
-          <span className="text-text-m">Robotic Enthusiast</span>
-        </div>
-      </section>
+          <Link href={"/"} className="text-text-m">
+            Coder Institute
+          </Link>
+          <Link href={"/"} className="text-text-m">
+            Robotic Enthusiast
+          </Link>
+        </section>
+      </main>
       <section className="flex justify-between font-medium">
         <span className="text-text-s font-medium">
           © 2023 Organisasi Kemahasiswaan Informatika FT-UH (OKIF FT-UH)
         </span>
-        <div className="flex gap-4 text-text-s">
-          <span>Instagram</span>
-          <span>Twitter</span>
-          <span>Linked In</span>
-        </div>
+        <ul className="flex gap-4 text-text-s">
+          <li>
+            <Link href={"/"}>Instagram</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Twitter</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Linked In</Link>
+          </li>
+        </ul>
       </section>
     </footer>
   );

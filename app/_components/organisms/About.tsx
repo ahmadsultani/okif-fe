0c16f6b84e2084d08cff8a47ceb21d2f8e0ab73f
naddiyh <nadtss12@gmail.com>
2023-08-27 18:00:06 +0800

    feat: 17 recreate about section

--- app/_components/organisms/About.tsx
+++ app/_components/organisms/About.tsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section className="w-full flex gap-8 px-16 py-12">
-      <Image
-        src="/image/about.webp"
-        width={519}
-        height={640}
-        alt="about-image"
-      />
-      <section className="flex flex-1 flex-col gap-8 text-justify tracking-[0.05em]">
+    <section className="w-full flex gap-8 pt-[72px] pb-12 px-16">
+      <section className="flex-1 relative">
+        <Image
+          fill
+          src={"/image/about.webp"}
+          alt="about-image"
+          style={{ objectFit: "cover", objectPosition: "top" }}
+        />
+      </section>
+      <section className="flex flex-1 flex-col gap-8 text-justify">
         <h1 className="font-bold text-heading-l">Mengenal OKIF FT-UH</h1>
-        <p className="font-medium text-heading-m">
+        <p className="font-medium text-heading-s">
           OKIF FT-UH adalah organisasi kemahasiswaan non-struktural di
           Departemen Teknik Informatika intra Universitas Hasanuddin yang
           berstatus otonom dan secara struktural merupakan bagian dari OKFT-UH
           yang mengacu pada bagan struktur OKFT-UH.
         </p>
-        <p className="font-medium text-heading-m">
+        <p className="font-medium text-heading-s">
           OKIF FT-UH bertujuan sebagai sarana peningkatan kualitas ketakwaan
           kepada Tuhan Yang Maha Esa, yang berasaskan nilai-nilai luhur
           pancasila demi terwujudnya cita-cita bangsa dengan mengembangkan

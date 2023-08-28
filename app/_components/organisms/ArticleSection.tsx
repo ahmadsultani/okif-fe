d5f011e2587f38c5753ee1f3ba1b85bf951cd5c9
Nurfadillah Umar <itsennuu@gmail.com>
2023-08-29 01:09:31 +0800

    feat: 11 article section

---
+++ app/_components/organisms/ArticleSection.tsx
@@ -0,0 +1,33 @@
+import ArticleCard from "@molecules/ArticleCard";
+
+const ArticleSection = () => {
+  return (
+    <main className="flex flex-col gap-8 w-screen px-16 py-12">
+      <section className=" flex justify-between  ">
+        <h1 className=" text-heading-l font-bold">Artikel</h1>
+        <p className=" text-heading-m font-medium text-neutral-normal-active hover:text-neutral-normal-hover">
+          Lihat Selengkapnya {"->"}
+        </p>
+      </section>
+      <section className="flex gap-[27px]">
+        <ArticleCard
+          date="Sabtu, 18 Maret 2023"
+          title="CODE (Community Dedication)"
+          description="Pengembangan potensi mahasiswa untuk turut serta dalam pengabdian kepada masyarakat dengan memanfaatkan teknologi informasi serta meningkatkan kepedulian dan rasa empati terhadap masyarakat yang membutuhkan bantuan."
+        />
+        <ArticleCard
+          date="Sabtu, 18 Maret 2023"
+          title="INFINITY (Informatics for Humanity)"
+          description="Gerakan kemanusiaan untuk membantu korban bencana alam."
+        />
+        <ArticleCard
+          date="Sabtu, 18 Maret 2023"
+          title="INFINITY (Informatics for Humanity)"
+          description="Gerakan kemanusiaan untuk membantu korban bencana alam.
+          "
+        />
+      </section>
+    </main>
+  );
+};
+export default ArticleSection;

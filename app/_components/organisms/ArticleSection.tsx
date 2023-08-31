27c623e0e642b4c163e9137034af5a23891f68b1
Nurfadillah Umar <itsennuu@gmail.com>
2023-08-31 18:37:08 +0800

    fix: 26 article section

--- app/_components/organisms/ArticleSection.tsx
+++ app/_components/organisms/ArticleSection.tsx
@@ -3,13 +3,13 @@ import ArticleCard from "@molecules/ArticleCard";
 const ArticleSection = () => {
   return (
     <main className="flex flex-col gap-8 w-screen px-16 py-12">
-      <section className=" flex justify-between  ">
+      <section className="flex items-center justify-between">
         <h1 className=" text-heading-l font-bold">Artikel</h1>
         <p className=" text-heading-m font-medium text-neutral-normal-active hover:text-neutral-normal-hover">
           Lihat Selengkapnya {"->"}
         </p>
       </section>
-      <section className="flex gap-[27px]">
+      <section className="grid grid-cols-3 gap-[27px]">
         <ArticleCard
           date="Sabtu, 18 Maret 2023"
           title="CODE (Community Dedication)"

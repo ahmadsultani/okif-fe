f3ef9928e5bfbe1a0e34dcc8eb1bbac2aee986e9
Nurfadillah Umar <itsennuu@gmail.com>
2023-08-31 18:37:08 +0800

    fix: 26 article section

--- app/_components/molecules/ArticleCard.tsx
+++ app/_components/molecules/ArticleCard.tsx
@@ -9,16 +9,27 @@ interface ArticleCardProps {
 const ArticleCard = (props: ArticleCardProps) => {
   const { date, title, description } = props;
   return (
-    <main className="relative w-[440px] h-[603px] rounded-lg overflow-hidden">
-      <Image src="/image/code.webp" fill objectFit="cover" alt="Image" />
-      <header className=" absolute flex flex-col justify-end gap-2 w-full h-full px-[19px] py-8 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.5)] text-secondary-white">
-        <section className="flex gap-2">
-          <Image src="/icon/date.svg" width={13} height={13} alt="date" />
-          <p className=" text-text-m">{date}</p>
-        </section>
-        <h1 className=" text-heading-s font-bold">{title}</h1>
-        <p className=" text-text-l line-clamp-1">{description}</p>
-      </header>
+    <main className="relative rounded-lg overflow-hidden">
+      <section className="w-[440px] h-[603px]">
+        <Image
+          fill
+          src="/image/code.webp"
+          priority
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+          alt={title}
+        />
+        <header className=" absolute flex flex-col justify-end gap-2 w-full h-full px-[19px] py-8 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.5)] text-secondary-white">
+          <section className="flex gap-2">
+            <Image src="/icon/date.svg" width={13} height={13} alt="date" />
+            <p className=" text-text-m">{date}</p>
+          </section>
+          <h1 className=" text-heading-s font-bold">{title}</h1>
+          <p className=" text-text-l line-clamp-1">{description}</p>
+        </header>
+      </section>
     </main>
   );
 };

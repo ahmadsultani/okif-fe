5f912dbdc140dbd3df5aa5047866d6a30249948e
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-09-10 12:34:19 +0800

    feat: 33 responsive hero

--- app/_components/organisms/Hero.tsx
+++ app/_components/organisms/Hero.tsx
@@ -1,29 +1,56 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import OkifLogo from "@atoms/OkifLogo";
 
 const Hero = () => {
+  const [logoSize, setLogoSize] = useState(308);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        //lg
+        setLogoSize(308);
+      } else if (window.innerWidth >= 768) {
+        //md
+        setLogoSize(231);
+      } else {
+        setLogoSize(154);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <section className="relative w-screen h-screen z-10">
-        <Image 
-          src="/image/code.webp"
-          alt="hero image"
-          fill
-          style={{
-            objectFit: 'cover',
-          }}
-          />
-        <div className="absolute bg-black bg-opacity-70 w-screen h-screen top-0 left-0"></div>
+    <section className="relative z-10 h-screen w-screen">
+      <Image
+        src="/image/code.webp"
+        alt="hero image"
+        fill
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <div className="absolute left-0 top-0 h-screen w-screen bg-black opacity-70"></div>
 
-        <section className="relative h-full flex flex-col gap-11 justify-center items-center">
-          <OkifLogo size={308} />
-          <p className="text-heading-l leading-[52px] font-semibold text-secondary-white text-center">
-            ORGANISASI KEMAHASISWAAN INFORMATIKA
+      <section className="relative flex h-full flex-col items-center justify-center gap-11">
+        <OkifLogo size={logoSize} />
+        <p className="text-center text-heading-s font-semibold text-secondary-white md:text-heading-m lg:text-heading-l lg:leading-[52px]">
+          ORGANISASI KEMAHASISWAAN INFORMATIKA
           <br />
-            FAKULTAS TEKNIK - UNIVERSITAS HASANUDDIN
-          </p>
-        </section>
+          FAKULTAS TEKNIK - UNIVERSITAS HASANUDDIN
+        </p>
+      </section>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
